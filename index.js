04f897313df139ffeaa88940ea76ebca7c83c8ab
Mikhail Vahrameev <vahrmih@yandex.ru>
2014-10-29 12:51:38 +0600

    Add cucumber dependency

--- index.js
+++ index.js
@@ -1,61 +1,27 @@
 'use strict';
 
-var es = require('event-stream');
-var spawn = require('child_process').spawn;
+var CucumberCli = require('cucumber').Cli;
 
-var winPath = 'cucumber-js';
-var unixPath = 'cucumber.js';
-
-var cucumber = function(options) {
-    var runOptions = [];
-    var binPath;
+module.exports = function(options, done) {
+    var runArgs = [];
 
     if (options.support) {
-        runOptions.push('-r');
-        runOptions.push(options.support);
+        runArgs.push('--require');
+        runArgs.push(options.support);
     }
 
     if (options.steps) {
-        runOptions.push('-r');
-        runOptions.push(options.steps);
+        runArgs.push('--require');
+        runArgs.push(options.steps);
     }
 
-    runOptions.push('--format');
-    runOptions.push(options.format || 'pretty');
+    runArgs.push('--format');
+    runArgs.push(options.format || 'pretty');
 
-    if (process.platform === 'win32') {
-        binPath = winPath;
-    } else {
-        binPath = unixPath;
-    }
+    runArgs.push(options.features || 'features');
 
-    var run = function(argument, callback) {
-        var filename = argument.path;
-
-        if (filename.indexOf('.feature') === -1) {
-            return callback();
-        }
-
-        var processOptions = runOptions.slice(0);
-        processOptions.push(filename);
-
-        var cli = spawn(binPath, processOptions);
-
-        var output = [];
-
-        cli.stdout.on('data', function(data) {
-            output.push(data);
-        });
-
-        cli.on('exit', function() {
-            var data = Buffer.concat(output).toString();
-            process.stdout.write(data.trim());
-        });
-
-        return callback();
-    };
-
-    return es.map(run);
+    var cli = CucumberCli(runArgs);
+    cli.run(function (succeeded) {
+        done(succeeded ? null : new Error('Cucumber is failed'));
+    });
 };
-
-module.exports = cucumber;
